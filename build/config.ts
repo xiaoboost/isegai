c384c90ebf61e30e19b988223713ace1e9545f56
xiaoboost <xiaoboost@qq.com>
2021-12-17 01:39:25 +0800

    fix: 修复网站资源引用路径错误

--- build/config.ts
+++ build/config.ts
@@ -5,4 +5,4 @@ export const assert = resolve('src/assets/');
 /** 构建输出的文件路径 */
 export const output = resolve('dist/');
 /** 调试时的公共路径 */
-export const publicPath = '/';
+export const publicPath = './';
